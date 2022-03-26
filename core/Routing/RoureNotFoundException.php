25a2e1a5ef42648f51201874fd857beb6389671f
Nguyễn Thế Mạnh <hoangtupro.a4@gmail.com>
2022-03-26 18:15:53 +0700

    return 404 not found if debug mode is off

--- core/Routing/RoureNotFoundException.php
+++ core/Routing/RoureNotFoundException.php
@@ -6,6 +6,12 @@ class RoureNotFoundException extends \Exception
 {
     public function __construct($requestUrl)
     {
+        if (!config('APP_DEBUG')) {
+            http_response_code(404);
+            echo "<h1>404 Not found</h1>";
+            die;
+        }
+
         parent::__construct("Route \"$requestUrl\" is not defined.");
     }
 }
