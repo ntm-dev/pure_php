71631329064e0d36a90597775832f4df066e467e
Nguyễn Thế Mạnh <hoangtupro.a4@gmail.com>
2022-04-12 13:38:34 +0700

    Change bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,5 @@
 require "./../vendor/autoload.php";
 require "functions.php";
 
-$configs = require "./../config/app.php";
-foreach ($configs['aliases'] as $alias => $class) {
-    class_alias($class, $alias);
-}
-unset($configs);
-
-require "./../routes/web.php";
-
-Route::dispatch();
+$app = Core\Application::getInstance();
+$app->dispatch();
