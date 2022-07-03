c51eb45952884d55729347f2f26198f329f16a43
Nguyễn Thế Mạnh <hoangtupro.a4@gmail.com>
2022-07-03 13:01:45 +0700

    add database config file

---
+++ config/database.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Database Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the database connections below you wish
+    | to use as your default connection for all database work. Of course
+    | you may use many connections at once using the Database library.
+    |
+    */
+
+    'default' => getenv('DB_CONNECTION') ?: 'mysql',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the database connections setup for your application.
+    | Of course, examples of configuring each database platform that is
+    | supported by Laravel is shown below to make development simple.
+    |
+    |
+    | All database work in Laravel is done through the PHP PDO facilities
+    | so make sure you have the driver for your particular database of
+    | choice installed on your machine before you begin development.
+    |
+    */
+
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => getenv('DB_HOST') ?: '127.0.0.1',
+            'port' => getenv('DB_PORT') ?: '3306',
+            'database' => getenv('DB_DATABASE') ?: 'forge',
+            'username' => getenv('DB_USERNAME') ?: 'forge',
+            'password' => getenv('DB_PASSWORD') ?: '',
+        ],
+    ],
+
+];
