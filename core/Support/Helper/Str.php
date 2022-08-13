15a8ce3a4d7eb27c5194c474b1cb62d0ae8d1bcd
Nguyễn Thế Mạnh <hoangtupro.a4@gmail.com>
2022-08-13 15:55:22 +0700

    add some string helper

--- core/Support/Helper/Str.php
+++ core/Support/Helper/Str.php
@@ -38,6 +38,34 @@ class Str
     }
 
     /**
+     * Check a string is empty.
+     *
+     * @param  string $value
+     * @param  bool   $trim
+     * @return string
+     */
+    public static function isEmpty($value, $trim = true)
+    {
+        if ($trim) {
+            $value = self::trim($value);
+        }
+
+        return empty($value) && $value !== "0";
+    }
+
+    /**
+     * Check a string is empty.
+     *
+     * @param  string $value
+     * @param  bool   $trim
+     * @return string
+     */
+    public static function isNotEmpty($value, $trim = true)
+    {
+        return !self::isEmpty($value, $trim);
+    }
+
+    /**
      * Left trim string.
      *
      * @param  string $value
@@ -240,6 +268,7 @@ class Str
      */
     public static function slug($title, $separator = '-')
     {
+        $title = static::ascii($title);
         // Convert all dashes/underscores into separator
         $flip = $separator === '-' ? '_' : '-';
 
@@ -255,6 +284,342 @@ class Str
         $title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
 
         return trim($title, $separator);
+    }
+
+    /**
+     * Checks if a string starts with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string  $needle
+     * @return bool
+     */
+    public static function startsWith($haystack, $needle)
+    {
+        return (string)$needle !== '' && self::strncmp($haystack, $needle, self::strlen($needle)) === 0;
+    }
+
+    /**
+     * Checks if a string ends with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string  $needle
+     * @return bool
+     */
+    public static function endsWith($haystack, $needle)
+    {
+        return $needle !== '' && self::substr($haystack, - self::strlen($needle)) === (string)$needle;
+    }
+
+    /**
+     * Determine if a string contains a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string  $needle
+     * @return bool
+     */
+    public static function contains($haystack, $needle)
+    {
+        return $needle !== '' && self::strpos($haystack, $needle) !== false;
+    }
+
+    /**
+     * Cut whitespace, break line, tab... in string.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function cutWhitespace($value)
+    {
+        return preg_replace("/[\s\n\r\t]/u", '', $value);
+    }
+
+    /**
+     * Replace a given value in the string sequentially with an array.
+     *
+     * @param  string  $search
+     * @param  array   $replace
+     * @param  string  $subject
+     * @return string
+     */
+    public static function replaceArray($search, array $replace, $subject)
+    {
+        foreach ($replace as $value) {
+            $subject = self::replaceFirst($search, $value, $subject);
+        }
+
+        return $subject;
+    }
+
+    /**
+     * Replace the first occurrence of a given value in the string.
+     *
+     * @param  string  $search
+     * @param  string  $replace
+     * @param  string  $subject
+     * @return string
+     */
+    public static function replaceFirst($search, $replace, $subject)
+    {
+        if ($search == '') {
+            return $subject;
+        }
+
+        $position = self::strpos($subject, $search);
+
+        if ($position !== false) {
+            return substr_replace($subject, $replace, $position, self::strlen($search));
+        }
+
+        return $subject;
+    }
+
+    /**
+     * Replace the last occurrence of a given value in the string.
+     *
+     * @param  string  $search
+     * @param  string  $replace
+     * @param  string  $subject
+     * @return string
+     */
+    public static function replaceLast($search, $replace, $subject)
+    {
+        $position = self::strrpos($subject, $search);
+
+        if ($position !== false) {
+            return substr_replace($subject, $replace, $position, self::strlen($search));
+        }
+
+        return $subject;
+    }
+
+    /**
+     * Begin a string with a single instance of a given value.
+     *
+     * @param  string  $value
+     * @param  string  $prefix
+     * @return string
+     */
+    public static function start($value, $prefix)
+    {
+        $quoted = preg_quote($prefix, '/');
+
+        return $prefix.preg_replace('/^(?:'.$quoted.')+/u', '', $value);
+    }
+
+    /**
+     * Convert the given string to upper-case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function upper($value)
+    {
+        return self::strtoupper($value, 'UTF-8');
+    }
+
+    /**
+     * Transliterate a UTF-8 value to ASCII.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function ascii($value)
+    {
+        foreach (static::charsArray() as $key => $val) {
+            $value = str_replace($val, $key, $value);
+        }
+
+        // replace space, ~
+        return preg_replace('/[^\x20-\x7E]/u', '', $value);
+    }
+
+    /**
+     * Check string is a valid Hankakus characters(Half-width kana).
+     *
+     * @param  mixed  $value
+     * @return bool
+     */
+    public static function isHankaku($value)
+    {
+        return !!preg_match('/^[\p{Han}]+$/u', $value);
+    }
+
+    /**
+     * Check string is a valid Katakana characters.
+     *
+     * @param  mixed  $value
+     * @return bool
+     */
+    public static function isKatakana($value)
+    {
+        return !!preg_match('/^[\p{Katakana}]+$/u', $value);
+    }
+
+    /**
+     * Check string is a valid Hiragana characters.
+     *
+     * @param  mixed  $value
+     * @return bool
+     */
+    public static function isHiragana($value)
+    {
+        return !!preg_match('/^[\p{Hiragana}]+$/u', $value);
+    }
+
+    /**
+     * Check string is a valid Furigana characters.
+     *
+     * @param  mixed  $value
+     * @return bool
+     */
+    public static function isFurigana($value)
+    {
+        return !!preg_match('/^[\p{Katakana}\p{Hiragana}]+$/u', $value);
+    }
+
+    /**
+     * Convert old chinese charactor to new charactor.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public static function convertOldChineseCharactor($value)
+    {
+        $oldCharactor = ["髙", "﨑", "栁", "桺", "㐂", "圡", "𡈽", "𠮷", "槗", "𣘺", "曻", "邉", "邊", "濵"];
+        $newCharactor = ["高", "崎", "柳", "柳", "喜", "土", "土", "吉", "橋", "橋", "昇", "辺", "辺", "濱"];
+
+        return str_replace($oldCharactor, $newCharactor, $value);
+    }
+
+    /**
+     * Returns the replacements for the ascii method.
+     *
+     * @see https://github.com/danielstjules/Stringy/blob/df24ab62d2d8213bbbe88cc36fc35a4503b4bd7e/src/Stringy.php#L1681
+     *
+     * @return array
+     */
+    protected static function charsArray()
+    {
+        static $charsArray;
+
+        if (isset($charsArray)) {
+            return $charsArray;
+        }
+
+        return $charsArray = [
+            '0'     => ['°', '₀', '۰', '０'],
+            '1'     => ['¹', '₁', '۱', '１'],
+            '2'     => ['²', '₂', '۲', '２'],
+            '3'     => ['³', '₃', '۳', '３'],
+            '4'     => ['⁴', '₄', '۴', '٤', '４'],
+            '5'     => ['⁵', '₅', '۵', '٥', '５'],
+            '6'     => ['⁶', '₆', '۶', '٦', '６'],
+            '7'     => ['⁷', '₇', '۷', '７'],
+            '8'     => ['⁸', '₈', '۸', '８'],
+            '9'     => ['⁹', '₉', '۹', '９'],
+            'a'     => ['à', 'á', 'ả', 'ã', 'ạ', 'ă', 'ắ', 'ằ', 'ẳ', 'ẵ', 'ặ', 'â', 'ấ', 'ầ', 'ẩ', 'ẫ', 'ậ', 'ā', 'ą', 'å', 'α', 'ά', 'ἀ', 'ἁ', 'ἂ', 'ἃ', 'ἄ', 'ἅ', 'ἆ', 'ἇ', 'ᾀ', 'ᾁ', 'ᾂ', 'ᾃ', 'ᾄ', 'ᾅ', 'ᾆ', 'ᾇ', 'ὰ', 'ά', 'ᾰ', 'ᾱ', 'ᾲ', 'ᾳ', 'ᾴ', 'ᾶ', 'ᾷ', 'а', 'أ', 'အ', 'ာ', 'ါ', 'ǻ', 'ǎ', 'ª', 'ა', 'अ', 'ا', 'ａ', 'ä'],
+            'b'     => ['б', 'β', 'ب', 'ဗ', 'ბ', 'ｂ'],
+            'c'     => ['ç', 'ć', 'č', 'ĉ', 'ċ', 'ｃ'],
+            'd'     => ['ď', 'ð', 'đ', 'ƌ', 'ȡ', 'ɖ', 'ɗ', 'ᵭ', 'ᶁ', 'ᶑ', 'д', 'δ', 'د', 'ض', 'ဍ', 'ဒ', 'დ', 'ｄ'],
+            'e'     => ['é', 'è', 'ẻ', 'ẽ', 'ẹ', 'ê', 'ế', 'ề', 'ể', 'ễ', 'ệ', 'ë', 'ē', 'ę', 'ě', 'ĕ', 'ė', 'ε', 'έ', 'ἐ', 'ἑ', 'ἒ', 'ἓ', 'ἔ', 'ἕ', 'ὲ', 'έ', 'е', 'ё', 'э', 'є', 'ə', 'ဧ', 'ေ', 'ဲ', 'ე', 'ए', 'إ', 'ئ', 'ｅ'],
+            'f'     => ['ф', 'φ', 'ف', 'ƒ', 'ფ', 'ｆ'],
+            'g'     => ['ĝ', 'ğ', 'ġ', 'ģ', 'г', 'ґ', 'γ', 'ဂ', 'გ', 'گ', 'ｇ'],
+            'h'     => ['ĥ', 'ħ', 'η', 'ή', 'ح', 'ه', 'ဟ', 'ှ', 'ჰ', 'ｈ'],
+            'i'     => ['í', 'ì', 'ỉ', 'ĩ', 'ị', 'î', 'ï', 'ī', 'ĭ', 'į', 'ı', 'ι', 'ί', 'ϊ', 'ΐ', 'ἰ', 'ἱ', 'ἲ', 'ἳ', 'ἴ', 'ἵ', 'ἶ', 'ἷ', 'ὶ', 'ί', 'ῐ', 'ῑ', 'ῒ', 'ΐ', 'ῖ', 'ῗ', 'і', 'ї', 'и', 'ဣ', 'ိ', 'ီ', 'ည်', 'ǐ', 'ი', 'इ', 'ی', 'ｉ'],
+            'j'     => ['ĵ', 'ј', 'Ј', 'ჯ', 'ج', 'ｊ'],
+            'k'     => ['ķ', 'ĸ', 'к', 'κ', 'Ķ', 'ق', 'ك', 'က', 'კ', 'ქ', 'ک', 'ｋ'],
+            'l'     => ['ł', 'ľ', 'ĺ', 'ļ', 'ŀ', 'л', 'λ', 'ل', 'လ', 'ლ', 'ｌ'],
+            'm'     => ['м', 'μ', 'م', 'မ', 'მ', 'ｍ'],
+            'n'     => ['ñ', 'ń', 'ň', 'ņ', 'ŉ', 'ŋ', 'ν', 'н', 'ن', 'န', 'ნ', 'ｎ'],
+            'o'     => ['ó', 'ò', 'ỏ', 'õ', 'ọ', 'ô', 'ố', 'ồ', 'ổ', 'ỗ', 'ộ', 'ơ', 'ớ', 'ờ', 'ở', 'ỡ', 'ợ', 'ø', 'ō', 'ő', 'ŏ', 'ο', 'ὀ', 'ὁ', 'ὂ', 'ὃ', 'ὄ', 'ὅ', 'ὸ', 'ό', 'о', 'و', 'θ', 'ို', 'ǒ', 'ǿ', 'º', 'ო', 'ओ', 'ｏ', 'ö'],
+            'p'     => ['п', 'π', 'ပ', 'პ', 'پ', 'ｐ'],
+            'q'     => ['ყ', 'ｑ'],
+            'r'     => ['ŕ', 'ř', 'ŗ', 'р', 'ρ', 'ر', 'რ', 'ｒ'],
+            's'     => ['ś', 'š', 'ş', 'с', 'σ', 'ș', 'ς', 'س', 'ص', 'စ', 'ſ', 'ს', 'ｓ'],
+            't'     => ['ť', 'ţ', 'т', 'τ', 'ț', 'ت', 'ط', 'ဋ', 'တ', 'ŧ', 'თ', 'ტ', 'ｔ'],
+            'u'     => ['ú', 'ù', 'ủ', 'ũ', 'ụ', 'ư', 'ứ', 'ừ', 'ử', 'ữ', 'ự', 'û', 'ū', 'ů', 'ű', 'ŭ', 'ų', 'µ', 'у', 'ဉ', 'ု', 'ူ', 'ǔ', 'ǖ', 'ǘ', 'ǚ', 'ǜ', 'უ', 'उ', 'ｕ', 'ў', 'ü'],
+            'v'     => ['в', 'ვ', 'ϐ', 'ｖ'],
+            'w'     => ['ŵ', 'ω', 'ώ', 'ဝ', 'ွ', 'ｗ'],
+            'x'     => ['χ', 'ξ', 'ｘ'],
+            'y'     => ['ý', 'ỳ', 'ỷ', 'ỹ', 'ỵ', 'ÿ', 'ŷ', 'й', 'ы', 'υ', 'ϋ', 'ύ', 'ΰ', 'ي', 'ယ', 'ｙ'],
+            'z'     => ['ź', 'ž', 'ż', 'з', 'ζ', 'ز', 'ဇ', 'ზ', 'ｚ'],
+            'aa'    => ['ع', 'आ', 'آ'],
+            'ae'    => ['æ', 'ǽ'],
+            'ai'    => ['ऐ'],
+            'ch'    => ['ч', 'ჩ', 'ჭ', 'چ'],
+            'dj'    => ['ђ', 'đ'],
+            'dz'    => ['џ', 'ძ'],
+            'ei'    => ['ऍ'],
+            'gh'    => ['غ', 'ღ'],
+            'ii'    => ['ई'],
+            'ij'    => ['ĳ'],
+            'kh'    => ['х', 'خ', 'ხ'],
+            'lj'    => ['љ'],
+            'nj'    => ['њ'],
+            'oe'    => ['œ', 'ؤ'],
+            'oi'    => ['ऑ'],
+            'oii'   => ['ऒ'],
+            'ps'    => ['ψ'],
+            'sh'    => ['ш', 'შ', 'ش'],
+            'shch'  => ['щ'],
+            'ss'    => ['ß'],
+            'sx'    => ['ŝ'],
+            'th'    => ['þ', 'ϑ', 'ث', 'ذ', 'ظ'],
+            'ts'    => ['ц', 'ც', 'წ'],
+            'uu'    => ['ऊ'],
+            'ya'    => ['я'],
+            'yu'    => ['ю'],
+            'zh'    => ['ж', 'ჟ', 'ژ'],
+            '(c)'   => ['©'],
+            'A'     => ['Á', 'À', 'Ả', 'Ã', 'Ạ', 'Ă', 'Ắ', 'Ằ', 'Ẳ', 'Ẵ', 'Ặ', 'Â', 'Ấ', 'Ầ', 'Ẩ', 'Ẫ', 'Ậ', 'Å', 'Ā', 'Ą', 'Α', 'Ά', 'Ἀ', 'Ἁ', 'Ἂ', 'Ἃ', 'Ἄ', 'Ἅ', 'Ἆ', 'Ἇ', 'ᾈ', 'ᾉ', 'ᾊ', 'ᾋ', 'ᾌ', 'ᾍ', 'ᾎ', 'ᾏ', 'Ᾰ', 'Ᾱ', 'Ὰ', 'Ά', 'ᾼ', 'А', 'Ǻ', 'Ǎ', 'Ａ', 'Ä'],
+            'B'     => ['Б', 'Β', 'ब', 'Ｂ'],
+            'C'     => ['Ç','Ć', 'Č', 'Ĉ', 'Ċ', 'Ｃ'],
+            'D'     => ['Ď', 'Ð', 'Đ', 'Ɖ', 'Ɗ', 'Ƌ', 'ᴅ', 'ᴆ', 'Д', 'Δ', 'Ｄ'],
+            'E'     => ['É', 'È', 'Ẻ', 'Ẽ', 'Ẹ', 'Ê', 'Ế', 'Ề', 'Ể', 'Ễ', 'Ệ', 'Ë', 'Ē', 'Ę', 'Ě', 'Ĕ', 'Ė', 'Ε', 'Έ', 'Ἐ', 'Ἑ', 'Ἒ', 'Ἓ', 'Ἔ', 'Ἕ', 'Έ', 'Ὲ', 'Е', 'Ё', 'Э', 'Є', 'Ə', 'Ｅ'],
+            'F'     => ['Ф', 'Φ', 'Ｆ'],
+            'G'     => ['Ğ', 'Ġ', 'Ģ', 'Г', 'Ґ', 'Γ', 'Ｇ'],
+            'H'     => ['Η', 'Ή', 'Ħ', 'Ｈ'],
+            'I'     => ['Í', 'Ì', 'Ỉ', 'Ĩ', 'Ị', 'Î', 'Ï', 'Ī', 'Ĭ', 'Į', 'İ', 'Ι', 'Ί', 'Ϊ', 'Ἰ', 'Ἱ', 'Ἳ', 'Ἴ', 'Ἵ', 'Ἶ', 'Ἷ', 'Ῐ', 'Ῑ', 'Ὶ', 'Ί', 'И', 'І', 'Ї', 'Ǐ', 'ϒ', 'Ｉ'],
+            'J'     => ['Ｊ'],
+            'K'     => ['К', 'Κ', 'Ｋ'],
+            'L'     => ['Ĺ', 'Ł', 'Л', 'Λ', 'Ļ', 'Ľ', 'Ŀ', 'ल', 'Ｌ'],
+            'M'     => ['М', 'Μ', 'Ｍ'],
+            'N'     => ['Ń', 'Ñ', 'Ň', 'Ņ', 'Ŋ', 'Н', 'Ν', 'Ｎ'],
+            'O'     => ['Ó', 'Ò', 'Ỏ', 'Õ', 'Ọ', 'Ô', 'Ố', 'Ồ', 'Ổ', 'Ỗ', 'Ộ', 'Ơ', 'Ớ', 'Ờ', 'Ở', 'Ỡ', 'Ợ', 'Ø', 'Ō', 'Ő', 'Ŏ', 'Ο', 'Ό', 'Ὀ', 'Ὁ', 'Ὂ', 'Ὃ', 'Ὄ', 'Ὅ', 'Ὸ', 'Ό', 'О', 'Θ', 'Ө', 'Ǒ', 'Ǿ', 'Ｏ', 'Ö'],
+            'P'     => ['П', 'Π', 'Ｐ'],
+            'Q'     => ['Ｑ'],
+            'R'     => ['Ř', 'Ŕ', 'Р', 'Ρ', 'Ŗ', 'Ｒ'],
+            'S'     => ['Ş', 'Ŝ', 'Ș', 'Š', 'Ś', 'С', 'Σ', 'Ｓ'],
+            'T'     => ['Ť', 'Ţ', 'Ŧ', 'Ț', 'Т', 'Τ', 'Ｔ'],
+            'U'     => ['Ú', 'Ù', 'Ủ', 'Ũ', 'Ụ', 'Ư', 'Ứ', 'Ừ', 'Ử', 'Ữ', 'Ự', 'Û', 'Ū', 'Ů', 'Ű', 'Ŭ', 'Ų', 'У', 'Ǔ', 'Ǖ', 'Ǘ', 'Ǚ', 'Ǜ', 'Ｕ', 'Ў', 'Ü'],
+            'V'     => ['В', 'Ｖ'],
+            'W'     => ['Ω', 'Ώ', 'Ŵ', 'Ｗ'],
+            'X'     => ['Χ', 'Ξ', 'Ｘ'],
+            'Y'     => ['Ý', 'Ỳ', 'Ỷ', 'Ỹ', 'Ỵ', 'Ÿ', 'Ῠ', 'Ῡ', 'Ὺ', 'Ύ', 'Ы', 'Й', 'Υ', 'Ϋ', 'Ŷ', 'Ｙ'],
+            'Z'     => ['Ź', 'Ž', 'Ż', 'З', 'Ζ', 'Ｚ'],
+            'AE'    => ['Æ', 'Ǽ'],
+            'Ch'    => ['Ч'],
+            'Dj'    => ['Ђ'],
+            'Dz'    => ['Џ'],
+            'Gx'    => ['Ĝ'],
+            'Hx'    => ['Ĥ'],
+            'Ij'    => ['Ĳ'],
+            'Jx'    => ['Ĵ'],
+            'Kh'    => ['Х'],
+            'Lj'    => ['Љ'],
+            'Nj'    => ['Њ'],
+            'Oe'    => ['Œ'],
+            'Ps'    => ['Ψ'],
+            'Sh'    => ['Ш'],
+            'Shch'  => ['Щ'],
+            'Ss'    => ['ẞ'],
+            'Th'    => ['Þ'],
+            'Ts'    => ['Ц'],
+            'Ya'    => ['Я'],
+            'Yu'    => ['Ю'],
+            'Zh'    => ['Ж'],
+            ' '     => ["\xC2\xA0", "\xE2\x80\x80", "\xE2\x80\x81", "\xE2\x80\x82", "\xE2\x80\x83", "\xE2\x80\x84", "\xE2\x80\x85", "\xE2\x80\x86", "\xE2\x80\x87", "\xE2\x80\x88", "\xE2\x80\x89", "\xE2\x80\x8A", "\xE2\x80\xAF", "\xE2\x81\x9F", "\xE3\x80\x80", "\xEF\xBE\xA0"],
+        ];
     }
 
     /**
