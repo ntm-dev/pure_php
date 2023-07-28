716a1c12064bf7e03dd5eded63fec79879ab962e
Nguyễn Thế Mạnh <hoangtupro.a4@gmail.com>
2023-07-28 17:48:37 +0700

    add config database migration path

--- config/database.php
+++ config/database.php
@@ -38,6 +38,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Migration Repository Table
+    |--------------------------------------------------------------------------
+    |
+    | This table keeps track of all the migrations that have already run for
+    | your application. Using this information, we can determine which of
+    | the migrations on disk haven't actually been run in the database.
+    |
+    */
+
+    'migrations' => 'databases/migrations',
+
+    /*
+    |--------------------------------------------------------------------------
     | Redis Databases
     |--------------------------------------------------------------------------
     */
